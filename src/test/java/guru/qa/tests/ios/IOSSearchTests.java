1657fb30726e9acb5b049b1a420b1271e389b44d
Nazilya <wkwad@mail.ru>
2023-02-12 20:35:15 +0300

    hw-22

---
+++ src/test/java/guru/qa/tests/ios/IOSSearchTests.java
@@ -0,0 +1,37 @@
+package guru.qa.tests.ios;
+
+import guru.qa.tests.TestBase;
+import io.qameta.allure.Owner;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static io.qameta.allure.Allure.step;
+import static java.lang.String.format;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.openqa.selenium.By.id;
+
+public class IOSSearchTests extends TestBase {
+    @Owner("Nazilya")
+    @Tag("ios")
+    @Test
+    void checkOutputTextTest() {
+        step("Click Text Button", () -> {
+            $(id("Text Button")).click();
+        });
+
+        step("Check initial state Output text", () -> {
+            assertEquals("Waiting for text input.", $(id("Text Output")).getText());
+        });
+
+        step(format("Set value %s in the input field and press enter", "Selenium"), () -> {
+            $(id("Text Input")).click();
+            $(id("Text Input")).sendKeys("Selenium");
+            $(id("Text Input")).pressEnter();
+        });
+
+        step("Check Output text", () -> {
+            assertEquals("Selenium", $(id("Text Output")).getText());
+        });
+    }
+}
